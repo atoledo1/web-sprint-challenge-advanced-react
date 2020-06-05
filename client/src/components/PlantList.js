fc074f8955144fbdf178700daf00db7b43a9ee4f
adriana <atoledosantiago1@gmail.com>
2020-06-05 16:03:37 -0500

    mvp

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  constructor(props) {
+    super(props);
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
-
-  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+    this.state = {
+      plants: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/plants")
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          plants: response.data.plantsData,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <main className="plant-list">
